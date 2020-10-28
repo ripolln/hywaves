ad17d24383fcf3e76d0776b5acf43a05ef434ba1
Nicolás Ripoll <ripollcab@gmail.com>
2020-10-28 11:14:39 +0100

    files reorder. each library (swan, swash, statistical) contains a "plots" sublibrary

--- hywaves/plotting/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import config
-from . import mda
-from . import swan
-from . import swash
-
--- hywaves/plotting/swash/__init__.py
+++
@@ -1,5 +0,0 @@
-#from . import plots
-
-# TODO: DECIDIR LOCALIZACION PLOTS
-#       plots incluido en swash? 
-#       plots aparte en plotting? 
